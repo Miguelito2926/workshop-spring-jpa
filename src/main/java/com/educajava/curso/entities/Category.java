e9829db61f562f564b973ae646d0ea702685ce75
ednaldo.junior <ednaldo.junior@prill.com.br>
2024-05-19 22:40:31 -0300

    Implement entity category

---
+++ src/main/java/com/educajava/curso/entities/Category.java
@@ -0,0 +1,46 @@
+package com.educajava.curso.entities;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+
+@ToString
+@EqualsAndHashCode
+
+
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
